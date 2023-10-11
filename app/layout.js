9686cedf4570a3ba86ecd485cdb53de68e709384
Anacgaraujo <ana.araujo@macys.com>
2023-10-11 19:57:44 -0400

    dark mode as default

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-import { ColorModeScript } from "@chakra-ui/react";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,11 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ColorModeScript initialColorMode="dark" />
-
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode="dark" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
